2bcc87d17eb1ca43bd5607c9dea2f563ccd77189
kenyaone254 <ivyn8384@gmail.com>
2023-03-20 07:46:53 -0700

    made django app

---
+++ shoesappdjango/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from shoes import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('shoes', views.shoes),
+    path('show', views.show),
+    path('edit/<int:id>', views.edit),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.destroy),
+    path('', views.show)
+
+]
